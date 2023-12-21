2d42b62189bd5878f97969c2d693ced1d53d0460
Hakan Lundvall <hakan.lundvall@gmail.com>
2023-12-21 15:18:27 +0100

    New dynamic API

--- include/bitpack.h
+++ include/bitpack.h
@@ -4,119 +4,217 @@
 #pragma once
 
 #include <algorithm>
+#include <concepts>
 #include <cstdint>
 #include <functional>
 #include <numeric>
+#include <ranges>
+#include <type_traits>
 
 namespace bitpack {
 
-constexpr uint32_t shift_left_and_add(const uint32_t a, const uint32_t b)
-{
-   return (a << 8) + b;
+constexpr uint32_t shift_left_and_add(const uint32_t a, const uint32_t b) {
+  return (a << 8) + b;
 }
 
-template <int pos, int bits, int... T>
-struct field
-{
-   static constexpr uint32_t start_index = pos / 8;
-   static constexpr uint32_t bit_offset = pos % 8;
-   static constexpr uint32_t byte_count = 1 + (bit_offset + bits - 1) / 8;
-   static constexpr uint32_t shift = byte_count * 8 - bit_offset - bits;
-   static constexpr uint32_t mask = uint32_t((uint64_t(1) << bits) - 1);
-   static constexpr uint32_t shifted_mask = (mask << shift);
+template <int pos, int bits, int... T> struct field {
+  static constexpr uint32_t start_index = pos / 8;
+  static constexpr uint32_t bit_offset = pos % 8;
+  static constexpr uint32_t byte_count = 1 + (bit_offset + bits - 1) / 8;
+  static constexpr uint32_t shift = byte_count * 8 - bit_offset - bits;
+  static constexpr uint32_t mask = uint32_t((uint64_t(1) << bits) - 1);
+  static constexpr uint32_t shifted_mask = (mask << shift);
 
-   // Set the value given in v in the buffer pointed to by the iterator first
-   // according to the bit pattern given in template arguments pos, bits, T...
-   template <class InputIt>
-   static constexpr void set(InputIt first, uint32_t v)
-   {
-      set_with_size(first, v);
-   }
+  // Set the value given in v in the buffer pointed to by the iterator first
+  // according to the bit pattern given in template arguments pos, bits, T...
+  template <class InputIt>
+  static constexpr void set(InputIt first, uint32_t v) {
+    set_with_size(first, v);
+  }
 
-   // Get the value from the buffer pointed to by the iterator first
-   // according to the bit pattern given in template arguments pos, bits, T...
-   template <class InputIt>
-   static constexpr uint32_t get(InputIt start)
-   {
-      return get_with_size(start).first;
-   }
+  // Get the value from the buffer pointed to by the iterator first
+  // according to the bit pattern given in template arguments pos, bits, T...
+  template <class InputIt> static constexpr uint32_t get(InputIt start) {
+    return get_with_size(start).first;
+  }
 
-   template <std::size_t S = 0, class InputIt>
-   static constexpr auto get_with_size(InputIt start) ->
+  template <std::size_t S = 0, class InputIt>
+  static constexpr auto get_with_size(InputIt start) ->
       typename std::enable_if_t<S == sizeof...(T),
-                                std::pair<uint32_t, uint32_t>>
-   {
-      return {do_get(start), bits};
-   }
+                                std::pair<uint32_t, uint32_t>> {
+    return {do_get(start), bits};
+  }
 
-   template <std::size_t S = 0, class InputIt>
-   static constexpr auto get_with_size(InputIt start) ->
+  template <std::size_t S = 0, class InputIt>
+  static constexpr auto get_with_size(InputIt start) ->
       typename std::enable_if_t<S != sizeof...(T),
-                                std::pair<uint32_t, uint32_t>>
-   {
-      auto res = field<T...>::get_with_size(start);
-      return {res.first + (do_get(start) << res.second), res.second + bits};
-   }
+                                std::pair<uint32_t, uint32_t>> {
+    auto res = field<T...>::get_with_size(start);
+    return {res.first + (do_get(start) << res.second), res.second + bits};
+  }
 
-   template <std::size_t S = 0, class InputIt>
-   static constexpr auto set_with_size(InputIt start, uint32_t v) ->
-      typename std::enable_if_t<S == sizeof...(T), uint32_t>
-   {
-      do_set(start, v);
-      return bits;
-   }
+  template <std::size_t S = 0, class InputIt>
+  static constexpr auto set_with_size(InputIt start, uint32_t v) ->
+      typename std::enable_if_t<S == sizeof...(T), uint32_t> {
+    do_set(start, v);
+    return bits;
+  }
 
-   template <std::size_t S = 0, class InputIt>
-   static constexpr auto set_with_size(InputIt start, uint32_t v) ->
-      typename std::enable_if_t<S != sizeof...(T), uint32_t>
-   {
-      auto res = field<T...>::set_with_size(start, v);
-      do_set(start, v >> res);
-      return res + bits;
-   }
+  template <std::size_t S = 0, class InputIt>
+  static constexpr auto set_with_size(InputIt start, uint32_t v) ->
+      typename std::enable_if_t<S != sizeof...(T), uint32_t> {
+    auto res = field<T...>::set_with_size(start, v);
+    do_set(start, v >> res);
+    return res + bits;
+  }
 
 private:
-   template <class InputIt>
-   static constexpr uint32_t do_get(InputIt start)
-   {
-      const auto start_pos = start + start_index;
-      const auto end_pos = start_pos + byte_count;
-      const auto part = std::accumulate(start_pos, end_pos, 0, shift_left_and_add);
-      return (part >> shift) & mask;
-   }
+  template <class InputIt> static constexpr uint32_t do_get(InputIt start) {
+    const auto start_pos = start + start_index;
+    const auto end_pos = start_pos + byte_count;
+    const auto part =
+        std::accumulate(start_pos, end_pos, 0, shift_left_and_add);
+    return (part >> shift) & mask;
+  }
 
-   template <class InputIt>
-   static constexpr void do_set(InputIt start, uint32_t v)
-   {
-      const auto start_pos = start + start_index;
-      const auto end_pos = start_pos + byte_count;
+  template <class InputIt>
+  static constexpr void do_set(InputIt start, uint32_t v) {
+    const auto start_pos = start + start_index;
+    const auto end_pos = start_pos + byte_count;
 
-      uint8_t c[4]{0};
+    uint8_t c[4]{0};
 
-      const uint32_t vc32{(v << shift) & shifted_mask};
-      const uint8_t vc[4]{uint8_t((vc32)&0xff),
-                          uint8_t((vc32 >> 8) & 0xff),
-                          uint8_t((vc32 >> 16) & 0xff),
-                          uint8_t((vc32 >> 24) & 0xff)};
+    const uint32_t vc32{(v << shift) & shifted_mask};
+    const uint8_t vc[4]{uint8_t((vc32)&0xff), uint8_t((vc32 >> 8) & 0xff),
+                        uint8_t((vc32 >> 16) & 0xff),
+                        uint8_t((vc32 >> 24) & 0xff)};
 
-      const uint32_t m32 = ~shifted_mask;
-      const uint8_t m[4]{uint8_t((m32)&0xff),
-                         uint8_t((m32 >> 8) & 0xff),
-                         uint8_t((m32 >> 16) & 0xff),
-                         uint8_t((m32 >> 24) & 0xff)};
+    const uint32_t m32 = ~shifted_mask;
+    const uint8_t m[4]{uint8_t((m32)&0xff), uint8_t((m32 >> 8) & 0xff),
+                       uint8_t((m32 >> 16) & 0xff),
+                       uint8_t((m32 >> 24) & 0xff)};
 
-      std::transform(start_pos,
-                     end_pos,
-                     std::crbegin(m) + (4 - byte_count),
-                     std::begin(c),
-                     std::bit_and<uint8_t>());
-      std::transform(std::cbegin(c),
-                     std::cbegin(c) + byte_count,
-                     std::rbegin(vc) + (4 - byte_count),
-                     start_pos,
-                     std::bit_or<uint8_t>());
-   }
+    std::transform(start_pos, end_pos, std::crbegin(m) + (4 - byte_count),
+                   std::begin(c), std::bit_and<uint8_t>());
+    std::transform(std::cbegin(c), std::cbegin(c) + byte_count,
+                   std::rbegin(vc) + (4 - byte_count), start_pos,
+                   std::bit_or<uint8_t>());
+  }
+};
+
+struct field_specifier {
+  constexpr field_specifier() = default;
+  constexpr field_specifier(const uint32_t pos, const uint32_t bits)
+      : pos{pos}, bits{bits} {}
+  uint32_t pos = 0;
+  uint32_t bits = 0;
+};
+
+class field_object {
+public:
+  template <typename Container>
+  requires std::ranges::sized_range<Container> &&
+      std::same_as<typename Container::value_type, field_specifier>
+  constexpr field_object(const Container &container)
+      : container_{copy(container)}, count_{container.size()} {}
+
+  template <class InputIt> constexpr void set(InputIt first, uint32_t v) const {
+    pack(container_, count_, first, v);
+  }
+
+  template <class InputIt> constexpr uint32_t get(const InputIt first) const {
+    return unpack(container_, count_, first);
+  }
+
+private:
+  template <typename Container, class InputIt>
+  requires std::ranges::sized_range<Container> &&
+      std::same_as<typename Container::value_type, field_specifier>
+  static constexpr void pack(const Container &fs, std::size_t count,
+                             InputIt start, uint32_t v) {
+    uint32_t result_shift = 0;
+    for (std::size_t i = std::min(count, sizeof(fs) / sizeof(field_specifier));
+         i > 0; --i) {
+      pack(fs[i - 1], start, v >> result_shift);
+      result_shift += fs[i - 1].bits;
+    }
+  }
+
+  template <class InputIt>
+  static constexpr void pack(const field_specifier fs, InputIt start,
+                             uint32_t v) {
+    const auto pos = fs.pos;
+    const auto bits = fs.bits;
+    const uint32_t start_index = pos / 8;
+    const uint32_t bit_offset = pos % 8;
+    const uint32_t byte_count = 1 + (bit_offset + bits - 1) / 8;
+    const uint32_t shift = byte_count * 8 - bit_offset - bits;
+    const uint32_t mask = uint32_t((uint64_t(1) << bits) - 1);
+    const uint32_t shifted_mask = (mask << shift);
+
+    const auto start_pos = start + start_index;
+    const auto end_pos = start_pos + byte_count;
+
+    uint8_t c[4]{0};
+
+    const uint32_t vc32{(v << shift) & shifted_mask};
+    const uint8_t vc[4]{uint8_t((vc32)&0xff), uint8_t((vc32 >> 8) & 0xff),
+                        uint8_t((vc32 >> 16) & 0xff),
+                        uint8_t((vc32 >> 24) & 0xff)};
+
+    const uint32_t m32 = ~shifted_mask;
+    const uint8_t m[4]{uint8_t((m32)&0xff), uint8_t((m32 >> 8) & 0xff),
+                       uint8_t((m32 >> 16) & 0xff),
+                       uint8_t((m32 >> 24) & 0xff)};
+
+    std::transform(start_pos, end_pos, std::crbegin(m) + (4 - byte_count),
+                   std::begin(c), std::bit_and<uint8_t>());
+    std::transform(std::cbegin(c), std::cbegin(c) + byte_count,
+                   std::rbegin(vc) + (4 - byte_count), start_pos,
+                   std::bit_or<uint8_t>());
+  }
+
+  template <typename Container, class InputIt>
+  requires std::ranges::sized_range<Container> &&
+      std::same_as<typename Container::value_type, field_specifier>
+  static constexpr uint32_t unpack(const Container &fs, std::size_t count,
+                                   InputIt start) {
+    uint32_t v{0};
+    uint32_t result_shift = 0;
+    for (auto i = std::min(count, sizeof(fs) / sizeof(field_specifier)); i > 0;
+         --i) {
+      v |= unpack(fs[i - 1], start, result_shift);
+      result_shift += fs[i - 1].bits;
+    }
+    return v;
+  }
+
+  template <class InputIt>
+  static constexpr uint32_t unpack(const field_specifier fs, InputIt start,
+                                   uint32_t result_shift) {
+    const auto pos = fs.pos;
+    const auto bits = fs.bits;
+    const uint32_t start_index = pos / 8;
+    const uint32_t bit_offset = pos % 8;
+    const uint32_t byte_count = 1 + (bit_offset + bits - 1) / 8;
+    const uint32_t shift = byte_count * 8 - bit_offset - bits;
+    const uint32_t mask = uint32_t((uint64_t(1) << bits) - 1);
+    const auto start_pos = start + start_index;
+    const auto end_pos = start_pos + byte_count;
+    const auto part =
+        std::accumulate(start_pos, end_pos, 0, shift_left_and_add);
+    return ((part >> shift) & mask) << result_shift;
+  }
+
+  template <typename Container>
+  constexpr std::array<field_specifier, 32> copy(const Container &container) {
+    std::array<field_specifier, 32> res;
+    std::copy(std::cbegin(container), std::cend(container), std::begin(res));
+    return res;
+  }
+
+  const std::array<field_specifier, 32> container_;
+  std::size_t count_;
 };
 
 } // namespace bitpack
-
--- test/main.cpp
+++ test/main.cpp
@@ -1,36 +1,61 @@
 // Copyright 2020 Håkan Lundvall
 // Licence: MIT
 
-
 #include "bitpack.h"
 
-// The template arguments of bitpack::field decide how the value given in field::set or retrieved in 
-// field::get is placed within the buffer.
-// The template arguments are, from right to left:
-// * how many bits to place
-// * start bit position within the buffer
-// repeat any number of times for fields that are non-contiguous  
+#include <algorithm>
+#include <gtest/gtest.h>
+#include <iostream>
+#include <random>
+#include <sstream>
 
-std::array<uint8_t, 4> global_buffer{};
-void set_32bit_LE(uint32_t v)
-{
-    bitpack::field<24,8,16,8,8,8,0,8>::set(begin(global_buffer), v);
-}
+namespace {
+constexpr std::array<uint8_t, 4> buffer{0x12, 0x34, 0x56, 0x78};
+constexpr std::array<bitpack::field_specifier, 7> fields{
+    bitpack::field_specifier{0, 4},  bitpack::field_specifier{8, 4},
+    bitpack::field_specifier{4, 4},  bitpack::field_specifier{16, 4},
+    bitpack::field_specifier{12, 4}, bitpack::field_specifier{20, 4},
+    bitpack::field_specifier{24, 8}};
+constexpr bitpack::field_object f{fields};
+static_assert(buffer.size() == 4);
+static_assert(f.get(cbegin(buffer)) == 0x13254678);
+} // namespace
 
-void set_16bit_BE(uint32_t v)
-{
-    bitpack::field<0,16>::set(begin(global_buffer), v);
-}
+TEST(BitpackTest, RandomValuesAndFields) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<uint32_t> dis(
+      1, 8); // field sizes from 1 to 8 bits
+  std::uniform_int_distribution<uint32_t> val_dis(0,
+                                                  UINT32_MAX); // random values
 
-int main()
-{
+  for (int i = 0; i < 100; ++i) { // run test 100 times
     std::array<uint8_t, 4> buffer{};
+    std::vector<bitpack::field_specifier> fields;
+    uint32_t total_bits = 0;
 
-    // set 8 bit value at pos 8 value in buffer: 00000000111111110000000000000000
-    //                                                   ^^^^^^^^
-    bitpack::field<8,8>::set(begin(buffer), 255);
+    while (total_bits < 32) {
+      auto field_size = dis(gen);
+      if (total_bits + field_size > 32) {
+        field_size = 32 - total_bits;
+      }
+      fields.push_back(bitpack::field_specifier{total_bits, field_size});
+      total_bits += field_size;
+    }
+    std::shuffle(fields.begin(), fields.end(), gen); // shuffle the fields
 
-    // get 8 bit value at pos 4 value in buffer: 00000000111111110000000000000000
-    //                                               ^^^^^^^^
-    return bitpack::field<4,8>::get(cbegin(buffer));
+    bitpack::field_object f{fields};
+    uint32_t v = val_dis(gen); // generate random value
+    f.set(begin(buffer), v);
+    auto b = f.get(cbegin(buffer));
+
+    std::stringstream ss;
+    ss << "Field specifications: " << std::endl;
+    for (const auto &field : fields) {
+      ss << "Start bit: " << field.pos << ", Field size: " << field.bits
+         << std::endl;
+    }
+
+    ASSERT_EQ(v, b) << ss.str() << std::endl;
+  }
 }
